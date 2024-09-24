93cfe21bf86ddf1d21bfee4a3f1bc3c55bf3c7b7
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-09-24 21:54:46 +0700

    no message

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       appearance={{ baseTheme: dark }}
       signInFallbackRedirectUrl="/"
       signUpFallbackRedirectUrl="/"
+      proxyUrl="https://driving-chipmunk-53.clerk.accounts.dev/__clerk"
     >
       <html lang="en">
         <body
