5ccf3b2821ec4fccd9cecb3ba2ec5a1d5c5291ef
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-09-24 22:08:35 +0700

    no message

--- middleware.ts
+++ middleware.ts
@@ -8,38 +8,33 @@ const isPublicRoute = createRouteMatcher([
   "/api/uploadthing",
 ]);
 
-export default clerkMiddleware(
-  (auth, request) => {
-    if (request.url.match("__clerk")) {
-      const proxyHeaders = new Headers(request.headers);
-      proxyHeaders.set(
-        "Clerk-Proxy-Url",
-        process.env.NEXT_PUBLIC_CLERK_PROXY_URL || ""
-      );
-      proxyHeaders.set("Clerk-Secret-Key", process.env.CLERK_SECRET_KEY || "");
-      if (request.ip) {
-        proxyHeaders.set("X-Forwarded-For", request.ip);
-      } else {
-        proxyHeaders.set(
-          "X-Forwarded-For",
-          request.headers.get("X-Forwarded-For") || ""
-        );
-      }
-
-      const proxyUrl = new URL(request.url);
-      proxyUrl.host = "frontend-api.clerk.dev";
-      proxyUrl.protocol = "https";
-      proxyUrl.pathname = proxyUrl.pathname.replace("/__clerk", "");
-
-      return NextResponse.rewrite(proxyUrl, {
-        request: {
-          headers: proxyHeaders,
-        },
-      });
-    }
-  },
-  { debug: true }
-);
+export default clerkMiddleware((auth, request) => {
+  //   if (request.url.match("__clerk")) {
+  //     const proxyHeaders = new Headers(request.headers);
+  //     proxyHeaders.set(
+  //       "Clerk-Proxy-Url",
+  //       process.env.NEXT_PUBLIC_CLERK_PROXY_URL || ""
+  //     );
+  //     proxyHeaders.set("Clerk-Secret-Key", process.env.CLERK_SECRET_KEY || "");
+  //     if (request.ip) {
+  //       proxyHeaders.set("X-Forwarded-For", request.ip);
+  //     } else {
+  //       proxyHeaders.set(
+  //         "X-Forwarded-For",
+  //         request.headers.get("X-Forwarded-For") || ""
+  //       );
+  //     }
+  //     const proxyUrl = new URL(request.url);
+  //     proxyUrl.host = "frontend-api.clerk.dev";
+  //     proxyUrl.protocol = "https";
+  //     proxyUrl.pathname = proxyUrl.pathname.replace("/__clerk", "");
+  //     return NextResponse.rewrite(proxyUrl, {
+  //       request: {
+  //         headers: proxyHeaders,
+  //       },
+  //     });
+  //   }
+});
 
 export const config = {
   matcher: [
